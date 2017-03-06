ece204bf448e24ed01fba89eb62bf9b74ad22859
Sirius <18201895526@163.com>
2017-03-06 12:10:11 +0800

    Update update.php

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"message":{"version":"600","versionCode":"106","url":"https://siriusm.github.io/Project.github.io/VuforiaDemo.apk","info":"update"}}
+{"message":{"version":"600","versionCode":"107","url":"https://siriusm.github.io/Project.github.io/VuforiaDemo.apk","info":"update"}}
